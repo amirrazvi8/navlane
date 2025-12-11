e17b873889543e4513b6d752da1bcf65ac024bee
mohd ayan <mohdayan7@gmail.com>
2025-12-11 23:46:00 +0530

    updating the css

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -6,7 +6,7 @@ import { CheckIcon, ChevronDownIcon, ChevronUpIcon } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 
-function Select({
+function Select({ 
   ...props
 }: React.ComponentProps<typeof SelectPrimitive.Root>) {
   return <SelectPrimitive.Root data-slot="select" {...props} />
