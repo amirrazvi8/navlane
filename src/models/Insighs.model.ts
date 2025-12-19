5f35b958cb3ed68b7d60a1c268d0e14bb8408660
mohd ayan <mohdayan7@gmail.com>
2025-12-20 01:04:28 +0530

    create the insight model

--- src/models/Insighs.model.ts
+++ src/models/Insighs.model.ts
@@ -1,4 +1,59 @@
-import mongoose from "mongoose";
- interface IInsight{
-    
- }
+import mongoose, { Schema } from "mongoose";
+interface IInsight {
+
+    trendingSkills?:{
+        skillName: string, 
+        trendScore: number, 
+        status: 'Hot' | 'Growing' | 'stable' | 'Declining'
+    }[];
+   
+    marketDemandAnalysis?:{
+        role: string, 
+        demandScore: number,
+        demandLevel: 'High Demand' | "Medium Demand" | "Low Demand"
+    }[];
+    competitionAnalysis?:{
+        role: string,
+        numberOfApplicants: number,
+        numberofJobs: number,
+        competitionLevel: 'Croweded' | 'Competitive' | 'Balanced' | 'Undercrowded' 
+    }[];
+
+}
+const InsightSchema = new Schema<IInsight>({
+    trendingSkills:[
+      { 
+        skillName:{ type: String, required: true },
+        trendScore:{ type: Number, required: true },
+        status:{ 
+            type: String,
+            enum: ['Hot', 'Growing', 'stable', 'Declining'],
+            required: true 
+        } 
+     }
+    ], 
+   
+    marketDemandAnalysis:[
+      {
+        role:{ type: String, required: true },
+        demandScore:{ type: Number, required: true },
+        demandLevel:{ 
+            type: String,
+            enum: ['High Demand', 'Medium Demand', 'Low Demand'],
+            required: true 
+        }
+      }
+    ],
+    competitionAnalysis:[
+      {
+        role:{ type: String, required: true },
+        numberOfApplicants:{ type: Number, required: true },
+        numberofJobs:{ type: Number, required: true },
+        competitionLevel:{ 
+            type: String,
+            enum: ['Croweded', 'Competitive', 'Balanced', 'Undercrowded'], required: true 
+        }
+      }
+    ]
+},{timestamps:true});
+export const InsightModel = mongoose.model<IInsight>('Insight', InsightSchema);
